09a4bfe0d8bc69c0d32ff2b67669ab38692a7c1e
gssygz <42406944+gssygz@users.noreply.github.com>
2019-12-30 09:31:26 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,14 +14,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "实高简报""
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "gssygz"
+email = "946233@qq.com"
+author_homepage = "9462393@qq.com"
+description = "只坚持一种正义。"
+key_words = ['实高简报', '广水实高']
 language = 'zh-CN'
 external_links = [
     {
@@ -50,24 +50,6 @@ nav = [
         "name": "关于",
         "url": "${site_prefix}about/",
         "target": "_self"
-    }
-]
-
-social_links = [
-    {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
-        "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
-        "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
     }
 ]
 
